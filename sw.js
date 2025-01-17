e03e54a02e0b5bb42633c2177757c57f53686fcc
ちと <chitose.dev@outlook.jp>
2025-01-17 20:53:25 +0900

    updata

--- sw.js
+++ sw.js
@@ -11,8 +11,15 @@ importScripts("./config/config.js");
 self.addEventListener("fetch",e=>{
     console.log(e.request);
     const urlObj=new URL(e.request.url);
-    const path=urlObj.pathname.replace(config.directory+config.scope,"");
-    console.log(decodeURIComponent(path));
-    e.request.url=decodeURIComponent(path);
-    return fetch(e.request);
+    const req_url=decodeURIComponent(urlObj.pathname.replace(config.directory+config.scope,""));
+    console.log(req_url);
+    e.respondWith((async()=>{
+      try{
+        const response=await fetch({...e.request,url:req_url});
+        return response;
+      }catch(e){
+        console.error("プロキシサーバーへのリクエスト失敗:", e);
+        return new Response("プロキシエラー", { status: 502 });
+      }
+    }));
 });
