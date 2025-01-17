601ff6da33636db7ef4930f90b048720ad0d9ebc
ちと <chitose.dev@outlook.jp>
2025-01-17 23:16:48 +0900

    updata

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 const config={
     directory:"/chitose-proxy/",
-    scope:"app/"
+    scope:"app/",
+    serverUrl:"https://chito-strage.glitch.me/req/"
 }
 if(typeof self =="object")self.config=config;
