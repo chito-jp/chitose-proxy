5535cf8523b6015a45b8b2bfb6bbfb5ab3502131
ちと <chitose.dev@outlook.jp>
2025-01-18 00:04:48 +0900

    updata

--- sw.js
+++ sw.js
@@ -9,7 +9,10 @@ self.addEventListener('activate', (event) => {
 importScripts("./config/config.js");
 
 self.addEventListener("fetch",e=>{
-  if(e.request.url.startsWith(config.serverUrl))return fetch(e.request);
+  console.log(String(e.request.url).startsWith(config.serverUrl))
+  if(e.request.url.startsWith(config.serverUrl)){
+    return fetch(e.request);
+  }
     const urlObj=new URL(e.request.url);
     const req_url=config.serverUrl+encodeURIComponent(urlObj.pathname.replace(config.directory+config.scope,""));
     e.respondWith((async () => {
