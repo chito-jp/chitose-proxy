3caaf37932cca433a6a30deced19d3fcfe738303
ちと <chitose.dev@outlook.jp>
2025-01-17 17:48:37 +0900

    updata

--- sv-register.js
+++ sv-register.js
@@ -1,6 +1,6 @@
 if("serviceWorker" in navigator){
     window.addEventListener("load",()=>{
-        navigator.serviceWorker.register(config.site.directory+"sw/sw.js",{scope:config.site.directory}).then(registration=>{
+        navigator.serviceWorker.register(config.site.directory+"sw/sw.js",{scope:"/chitose-proxy/"}).then(registration=>{
             console.log("サービスワーカーの登録に成功しました");
         }).catch(error=>{
             console.error("サービスワーカーの登録に失敗しました",error)
