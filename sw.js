9c453d7819613f2931ad384246dbd87bb1631cea
ちと <chitose.dev@outlook.jp>
2025-01-17 21:35:49 +0900

    updata

--- sw.js
+++ sw.js
@@ -9,12 +9,12 @@ self.addEventListener('activate', (event) => {
 importScripts("./config/config.js");
 
 self.addEventListener("fetch",e=>{
-    console.log(e.request);
     const urlObj=new URL(e.request.url);
     const req_url=decodeURIComponent(urlObj.pathname.replace(config.directory+config.scope,""));
     e.respondWith((async () => {
       e.request.url=req_url;
       console.log(e.request);
-      return fetch(e.request);
+      console.log(req_url)
+      return fetch(req_url);
     })(),);
 });
