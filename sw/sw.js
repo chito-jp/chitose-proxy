0e2c554942086ce60f634b2b3cdc415d4a9617db
ちと <chitose.dev@outlook.jp>
2025-01-17 18:18:23 +0900

    updata

--- sw/sw.js
+++ sw/sw.js
@@ -1,3 +1,11 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // 新しいサービスワーカーが即座に有効になるように
+});
+  
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim()); // 現在のクライアントに対してすぐにサービスワーカーを適用
+});
+
 self.addEventListener("fetch",e=>{
     console.log(e.request);
     return fetch(e.request);
