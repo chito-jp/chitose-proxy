423a22035634dbe60ea0a6644f0ff4b352b18a87
ちと <chitose.dev@outlook.jp>
2025-01-17 17:02:56 +0900

    configファイルの追加、パスの修正

---
+++ config/config.js
@@ -0,0 +1,5 @@
+const config={
+    site:{
+        directory:"/chitose-proxy/"
+    }
+}
--- sv-register.js
+++ sv-register.js
@@ -1,6 +1,6 @@
 if("serviceWorker" in navigator){
     window.addEventListener("load",()=>{
-        navigator.serviceWorker.register("../sw/sw.js").then(registration=>{
+        navigator.serviceWorker.register(config.site.directory+"sw/sw.js").then(registration=>{
             console.log("サービスワーカーの登録に成功しました");
         }).catch(error=>{
             console.error("サービスワーカーの登録に失敗しました")
