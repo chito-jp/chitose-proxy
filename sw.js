e8398580a0d5c3921d32cb1f32fe9f6787beeb89
ちと <chitose.dev@outlook.jp>
2025-01-18 11:28:59 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,17 @@
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
   
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
 importScripts("./sw/config.js");
 
 self.addEventListener("fetch",e=>{
-  console.log(e.request.url);
-  console.log(String(e.request.url).startsWith(config.serverUrl))
-  if(e.request.url.startsWith(config.serverUrl)){
+  e.respondWith((async()=>{
+    const cachedResponse = await caches.match(e.request);
+    if(cachedResponse)return cachedResponse;
     return fetch(e.request);
-  }
-    const urlObj=new URL(e.request.url);
-    const req_url=config.serverUrl+encodeURIComponent(urlObj.pathname.replace(config.directory+config.scope,""));
-    e.respondWith((async () => {
-      const request=new Request(req_url,{
-        ...e.request
-      });
-      console.log(request);
-      console.log(req_url)
-      return await fetch(req_url);
-    })(),);
+  }));
 });
